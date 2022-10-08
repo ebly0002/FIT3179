edab14fbc99bf8a882c04be6fd2881c4e4487d5a
ebly0002 <114212058+ebly0002@users.noreply.github.com>
2022-10-08 23:31:24 +1100

    Update vega_lite_vis.js

--- Week 10/vega_lite_vis.js
+++ Week 10/vega_lite_vis.js
@@ -1,9 +1,6 @@
-var vg_1 = "terrorist_attacks.vg.json";
-vegaEmbed("#terrorist_attacks_map", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
 
-var vg_2 = "filtered_graph.vg.json";
-vegaEmbed("#filtered_graph", vg_2).then(function(result) {
+
+var vg_1 = "filtered_graph.vg.json";
+vegaEmbed("#filtered_graph", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
