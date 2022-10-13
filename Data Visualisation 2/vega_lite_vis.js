00b077dd25ecebba75f6945b0236c7604f57282c
ebly0002 <114212058+ebly0002@users.noreply.github.com>
2022-10-13 13:01:21 +1100

    Create vega_lite_vis.js

---
+++ Data Visualisation 2/vega_lite_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "terrorist_attacks.vg.json";
+vegaEmbed("#terrorist_attacks_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "stacked_attack_graph.vg.json";
+vegaEmbed("#stacked_attack_graph", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "suicide_rates.vg.json";
+vegaEmbed("#suicide_rates", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
