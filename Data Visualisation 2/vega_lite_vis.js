51e191bc6c6e79faad8a0719e749ea9c023d446b
ebly0002 <114212058+ebly0002@users.noreply.github.com>
2022-10-14 09:48:34 +1100

    Update vega_lite_vis.js

--- Data Visualisation 2/vega_lite_vis.js
+++ Data Visualisation 2/vega_lite_vis.js
@@ -18,3 +18,7 @@ vegaEmbed("#weapons", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_4 = "unfinished_map.vg.json";
+vegaEmbed("#unfinished_map", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
