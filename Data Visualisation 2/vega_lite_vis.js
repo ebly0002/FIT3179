6d97d9c542bcff282ab6d895f2f12cd0f7451a9b
ebly0002 <114212058+ebly0002@users.noreply.github.com>
2022-10-14 16:51:14 +1100

    Update vega_lite_vis.js

--- Data Visualisation 2/vega_lite_vis.js
+++ Data Visualisation 2/vega_lite_vis.js
@@ -22,3 +22,8 @@ var vg_5 = "choropleth_test.vg.json";
 vegaEmbed("#choropleth_map", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_6 = "choropleth_test2.vg.json";
+vegaEmbed("#choropleth_map2", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
